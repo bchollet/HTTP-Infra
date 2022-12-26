20bba32d19ea344b73c982a5651e12b5904d5ac1
Bastian Chollet <bastian.chollet@mobi.ch>
2022-12-26 17:57:07 +0100

    Added jQuery + AJAX script

--- http-static/src/js/pets.js
+++ http-static/src/js/pets.js
@@ -1,3 +1,28 @@
-fetch('http://localhost:9091')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+$(function() {
+    //Function declaration
+    function loadPets() {
+        let pet = [];
+        let petGender;
+
+        fetch('http://localhost/api') // Returns HTTP Response as a Promise
+            .then((response) => response.json()) // Extracts body of HTTP response as a Promise
+            .then((data) => pet = data[0]);  // Get the raw data from body
+        
+        if(pet.length > 0) {
+            $('#first-name-span').text(pet.firstName);
+            $('#type-p').text('I am a cute little ' + pet.type);
+
+            petGender = pet.gender === 'Male' ? 'assets/img/male.jpg' : 'assets/img/female.jpg';
+            console.log('path to img: ', petGender);
+
+            $('#gender-img').attr('src', petGender);
+            $('#age-p').text(pet.age);
+            $('#weight-p').text(pet.weight);
+        } else  {
+            $('#first-name-span').text('No pet loaded :(');
+        }
+    }
+
+    //Function call
+    setInterval(loadPets(), 10000);
+});
